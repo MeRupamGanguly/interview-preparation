ade20d807caae50aef9fed835776a7c98366b1a3
Rupam Ganguly <merupamganguly@gmail.com>
2025-05-02 03:57:41 +0530

    closure

--- Golang.md
+++ Golang.md
@@ -158,20 +158,21 @@ A closure is a special type of anonymous function that can access/use variables,
 
 ```go
 func main() {
-	a := func(i int) int { return i * i }
+	a := func(i int) int { return i * i } // A function that takes an int and returns its square.
 	v := func(f func(i int) int) func() int {
-		fmt.Println("Inside : Address of f", &f)
+		fmt.Println("Inside : Address of f", &f) // This is the address of the function parameter `f`, which is a local copy of the function `a` passed into `v`.
 		c := f(3)
 		return func() int {
-			fmt.Println("Inside : Address of c", &c, " Value ", c)
+			fmt.Println("Inside : Address of c", &c, " Value ", c) // This is the address of the local variable `c` inside `v`. When the closure is returned, it captures `c` and keeps it in its own memory space.
 			c++
 			return c
 		}
 	}
-	f := v(a) // v() returns a function address
-	fmt.Println("Inside f is ", f)
-	fmt.Println("f Function Called and get", f()) // call the function which v() returns
+	f := v(a)                                     // v() returns a function address
+	fmt.Println("Inside f is ", f)                // This is the address of the returned closure function, func() int. It’s a separate function, dynamically created and returned by v.
+	fmt.Println("f Function Called and get", f()) // call the function which v() returns // When f() is called, it returns c+1, showing that the closure is preserving the state of c between invocations (i.e., it's a closure over c).
 }
+
 ```
 
 ```bash
--- cmd/main.go
+++ cmd/main.go
@@ -2,52 +2,27 @@ package main
 
 import (
 	"fmt"
-	"log"
 )
 
 func main() {
-	arr := []int{2, 0, 6, 0, 9, 12, 0, 46}
-	x := Zero2Front(arr)
-	arr = []int{2, 0, 6, 0, 9, 12, 0, 46}
-	y := Zero2FrontOrdered(arr)
-	fmt.Println(x, y)
+	a := func(i int) int { return i * i } // A function a that takes an int and returns its square.
+	v := func(f func(i int) int) func() int {
+		fmt.Println("Inside : Address of f", &f) // This is the address of the function parameter f, which is a local copy of the function a passed into v.
+		c := f(3)
+		return func() int {
+			fmt.Println("Inside : Address of c", &c, " Value ", c) // This is the address of the local variable c inside v. When the closure is returned, it captures c and keeps it in its own memory space.
+			c++
+			return c
+		}
+	}
+	f := v(a)                                     // v() returns a function address
+	fmt.Println("Inside f is ", f)                // This is the address of the returned closure function, func() int. It’s a separate function, dynamically created and returned by v.
+	fmt.Println("f Function Called and get", f()) // call the function which v() returns // When f() is called, it returns c+1, showing that the closure is preserving the state of c between invocations (i.e., it's a closure over c).
 }
-func Zero2FrontOrdered(arr []int) []int {
-	l := 0
-	r := 0
-	for range arr {
-		if arr[r] == 0 {
 
-			if r-l == 1 {
-				arr[r] = arr[l]
-				arr[l] = 0
-			} else {
-				rr := r
-				for range arr[l : rr+1] {
-					arr[rr] = arr[rr-1]
-					rr--
-				}
-				arr[l] = 0
-			}
-			l++
-		}
-		r++
-	}
-	return arr
-}
-func Zero2Front(arr []int) []int {
-	l := 0
-	r := 0
-	loopCount := 0
-	for range arr {
-		loopCount++
-		if arr[r] == 0 {
-			arr[r] = arr[l]
-			arr[l] = 0
-			l++
-		}
-		r++
-	}
-	log.Println("Iteration Number", loopCount)
-	return arr
-}
+/*
+Inside : Address of f 0xc000066040
+Inside f is  0x4b1f40
+Inside : Address of c 0xc000010120  Value  9
+f Function Called and get 10
+*/
